28722017ac5a68c668b05c829d6492e67a32fd0f
StevenRB <retanasteven8@gmail.com>
2023-11-06 23:15:20 -0600

    Initial Commit

---
+++ src/main/java/com/hospitalexpress/HospitalExpressApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalexpress/HospitalExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
