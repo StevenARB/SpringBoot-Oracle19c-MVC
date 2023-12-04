188be09b32f7233febc3c06495d491cdf556f7c1
Catalina Valverde <catvalra@gmail.com>
2023-12-03 21:13:44 -0600

    Repository citas

--- src/main/java/com/hospitalexpress/model/Cita.java
+++ src/main/java/com/hospitalexpress/model/Cita.java
@@ -4,10 +4,86 @@
  */
 package com.hospitalexpress.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.sql.Date;
+
 /**
  *
  * @author Cata
  */
+
+@Entity
+@Table(name = "cita", schema = "hospitalexpress")
 public class Cita {
+    
+    @Id
+    
+    private Integer id_cita, id_doctor, id_paciente;
+    private String tipo, estado;
+    private Date fecha_hora;
+    
+    @OneToOne
+    @JoinColumn(name = "id_cita")
+    private Cita cita;
+
+    public Integer getId_cita() {
+        return id_cita;
+    }
+
+    public void setId_cita(Integer id_cita) {
+        this.id_cita = id_cita;
+    }
+
+    public Integer getId_doctor() {
+        return id_doctor;
+    }
+
+    public void setId_doctor(Integer id_doctor) {
+        this.id_doctor = id_doctor;
+    }
+
+    public Integer getId_paciente() {
+        return id_paciente;
+    }
+
+    public void setId_paciente(Integer id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFecha_hora() {
+        return fecha_hora;
+    }
+
+    public void setFecha_hora(Date fecha_hora) {
+        this.fecha_hora = fecha_hora;
+    }
+
+    public Cita getCita() {
+        return cita;
+    }
+
+    public void setCita(Cita cita) {
+        this.cita = cita;
+    }    
     
 }
