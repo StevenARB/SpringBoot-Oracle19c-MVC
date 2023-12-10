9d3f565b4acd4755685c5477e91fd05f6b5b98c1
StevenRB <retanasteven8@gmail.com>
2023-12-10 15:28:56 -0600

    Páginas de Usuario

--- src/main/java/com/hospitalexpress/controller/ProductoController.java
+++ src/main/java/com/hospitalexpress/controller/ProductoController.java
@@ -27,86 +27,85 @@ public class ProductoController {
         }
         return "producto/producto";
     }
-    
+
     @GetMapping("/productos")
-public String findProductos(Model model) {
-    try {
-        List<Producto> listProductos = productoService.getProductos();
-        if (listProductos != null) {
-            model.addAttribute("productos", listProductos);
-        } else {
+    public String findProductos(Model model) {
+        try {
+            List<Producto> listProductos = productoService.getProductos();
+            if (listProductos != null) {
+                model.addAttribute("productos", listProductos);
+            } else {
+                model.addAttribute("listaVacia", true);
+            }
+        } catch (Exception e) {
             model.addAttribute("listaVacia", true);
         }
-    } catch (Exception e) {
-        model.addAttribute("listaVacia", true);
+        return "producto/productos";
     }
-    return "producto/productos";
-}
- 
-@GetMapping("/producto/insertar")
-public String insertarProducto(Model model) {
-    model.addAttribute("producto", new Producto());
-    return "producto/insertar";
-}
 
-@PostMapping("/producto/insertar")
-public String insertarProducto(Model model, @ModelAttribute Producto producto) {
-    try {
-        String result = productoService.insertarProducto(
-            producto.getNombre(),
-            producto.getDescripcion(),
-            producto.getCantidad(),
-            producto.getPrecio()
-        );
-        model.addAttribute("resultado", result);
-    } catch (Exception e) {
-        model.addAttribute("error", true);
+    @GetMapping("/producto/insertar")
+    public String insertarProducto(Model model) {
+        model.addAttribute("producto", new Producto());
+        return "producto/insertar";
     }
-    return "producto/insertar";
-}
-    
-@GetMapping("/producto/actualizar/{id}")
-public String findProductoByIdToUpdate(Model model, @PathVariable Integer id) {
-    try {
-        Producto producto = productoService.getProductoById(id);
-        if (producto != null) {
-            model.addAttribute("producto", producto);
-        } else {
+
+    @PostMapping("/producto/insertar")
+    public String insertarProducto(Model model, @ModelAttribute Producto producto) {
+        try {
+            String result = productoService.insertarProducto(
+                    producto.getNombre(),
+                    producto.getDescripcion(),
+                    producto.getCantidad(),
+                    producto.getPrecio()
+            );
+            model.addAttribute("resultado", result);
+        } catch (Exception e) {
+            model.addAttribute("error", true);
+        }
+        return "producto/insertar";
+    }
+
+    @GetMapping("/producto/actualizar/{id}")
+    public String findProductoByIdToUpdate(Model model, @PathVariable Integer id) {
+        try {
+            Producto producto = productoService.getProductoById(id);
+            if (producto != null) {
+                model.addAttribute("producto", producto);
+            } else {
+                model.addAttribute("productoNoEncontrado", true);
+            }
+        } catch (Exception e) {
             model.addAttribute("productoNoEncontrado", true);
         }
-    } catch (Exception e) {
-        model.addAttribute("productoNoEncontrado", true);
+        return "producto/actualizar";
     }
-    return "producto/actualizar";
-}
 
-@PostMapping("/producto/actualizar/{id}")
-public String actualizarProducto(Model model, @PathVariable Integer id, @ModelAttribute Producto producto) {
-    try {
-        String result = productoService.actualizarProducto(
-            id,
-            producto.getNombre(),
-            producto.getDescripcion(),
-            producto.getCantidad(),
-            producto.getPrecio()
-        );
-        model.addAttribute("resultado", result);
-    } catch (Exception e) {
-        model.addAttribute("error", true);
+    @PostMapping("/producto/actualizar/{id}")
+    public String actualizarProducto(Model model, @PathVariable Integer id, @ModelAttribute Producto producto) {
+        try {
+            String result = productoService.actualizarProducto(
+                    id,
+                    producto.getNombre(),
+                    producto.getDescripcion(),
+                    producto.getCantidad(),
+                    producto.getPrecio()
+            );
+            model.addAttribute("resultado", result);
+        } catch (Exception e) {
+            model.addAttribute("error", true);
+        }
+        return "redirect:/productos";
     }
-    return "redirect:/productos";
-}
 
-@GetMapping("/producto/eliminar/{id}")
-public String eliminarProducto(Model model, @PathVariable Integer id) {
-    try {
-        String result = productoService.eliminarProducto(id);
-        model.addAttribute("resultado", result);
-    } catch (Exception e) {
-        model.addAttribute("error", true);
+    @GetMapping("/producto/eliminar/{id}")
+    public String eliminarProducto(Model model, @PathVariable Integer id) {
+        try {
+            String result = productoService.eliminarProducto(id);
+            model.addAttribute("resultado", result);
+        } catch (Exception e) {
+            model.addAttribute("error", true);
+        }
+        return "redirect:/productos";
     }
-    return "redirect:/productos";
-}
-
 
 }
